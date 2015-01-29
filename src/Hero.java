1ab1700965dd0f28754d716b268b69c45d7f0dd0
TomPickett <Pickett.tom7@gmail.com>
2015-01-29 01:10:55 -0500

    Created Hero Class

---
+++ src/Hero.java
@@ -0,0 +1,13 @@
+/**
+ * @author Tom Pickett
+ *Class Description: The main Hero 
+ *@Version 0.1 - Created Hero class
+ */
+public class Hero {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
