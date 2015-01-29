e5adf7b23b7073b8a3501e26cd6b3bca6468d69d
TomPickett <Pickett.tom7@gmail.com>
2015-01-29 01:45:35 -0500

    Almost finished Hero class. implemented Hero class in main method.

--- src/Hero.java
+++ src/Hero.java
@@ -5,5 +5,65 @@
  */
 
 public class Hero {
+	
+	//Private properties ***********************
+	private int strength;
+	private int speed;
+	private int health;
+	
+	//Public Properties ***********************
+	public String name;
+	
+	//Getters *********************************
+	public int getStrength() {
+		return strength;
+	}
 
+
+	public int getSpeed() {
+		return speed;
+	}
+
+
+	public int getHealth() {
+		return health;
+	}
+
+	//Constructor *****************************
+	public Hero(String name) {
+		this.name = name;
+		
+		generateAbilities();
+	}
+
+	
+	//Private Methods *************************
+	private void generateAbilities() {
+		this.strength = (int)(Math.random() * 100 + 1);
+		this.speed = (int)(Math.random() * 100 + 1);
+		this.health = (int)(Math.random() * 100 + 1);
+	}
+	
+	private void hitAttempt() {
+		
+	}
+	
+	private void hitDamage() {
+		
+	}
+	
+	//Public Methods **************************
+	public void showAbilities() {
+		System.out.println("***************************");
+		System.out.println("Strength: " + this.strength);
+		System.out.println("Speed: " + this.speed);
+		System.out.println("Health: " + this.health);
+		System.out.println("***************************");
+	}
+	
+	public void fight() {
+		System.out.println(this.name + " is fighting");
+		
+	}
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 /**
  * @author Tom Pickett
  *Class Description: The main part of the project
- *@Version 0.1 - Created Hero class
+ *@Version 0.2 - Almost finished Hero class
  */
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		//Instantiate Hero
+		Hero hero = new Hero("Hercules");
+		hero.showAbilities();
+		hero.fight();
+		
 	}
 
 }
