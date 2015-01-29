2f43242140b7a60ccac928c0dfcff299f43a4e7e
TomPickett <Pickett.tom7@gmail.com>
2015-01-29 01:15:33 -0500

    Actually Created Hero class and a main class.

--- src/Hero.java
+++ src/Hero.java
@@ -1,13 +1,9 @@
 /**
  * @author Tom Pickett
  *Class Description: The main Hero 
- *@Version 0.1 - Created Hero class
+ *
  */
+
 public class Hero {
-
-	public static void main(String[] args) {
-		
-
-	}
 
 }
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author Tom Pickett
+ *Class Description: The main part of the project
+ *@Version 0.1 - Created Hero class
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
